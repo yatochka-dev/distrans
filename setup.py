cc611e9815084daaf095bf82bce75d0cf91dec10
saaasuuukaae <philipchef13@gmail.com>
2022-11-06 20:00:34 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     long_description = f.read()
 
-VERSION = "0.0.1"
+VERSION = "1.0.0"
 DESCRIPTION = "A simple translation library for discord bots"
 
 setup(
